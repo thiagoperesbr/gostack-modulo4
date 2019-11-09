78dd65546a1526a9c70f6ded03c10bc3566e3870
Thiago Peres <thiago.mourao.peres@gmail.com>
2019-11-09 15:43:39 -0300

    Importando CSS e Imagens

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import './App.css';
+
+import avatar from './assets/avatar.jpg';
 
 function App() {
-  return <h1>Hello Rocketseat</h1>;
+  return <img width="200" src={avatar} />;
 }
 
 export default App;
