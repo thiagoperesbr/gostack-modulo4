ed7747f2be6df8b31b0887a71f6f940b7602fd68
Thiago Peres <thiago.mourao.peres@gmail.com>
2019-11-09 16:22:40 -0300

    Class Components, Estado e Imutabilidade

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './App.css';
 
-import avatar from './assets/avatar.jpg';
+import TechList from './components/TechList';
 
 function App() {
-  return <img width="200" src={avatar} />;
+  return <TechList />;
 }
 
 export default App;
